f651075291879dfc0a3009ec80fb8b8370eba1a2
gustavo <gustavojuliao1999@gmail.com>
2022-05-06 10:27:13 -0300

    Bugs Fixeds, Load image, Save Image, Slow Produtos loading, config list, Page Edit add view image, Edit Create Alert if Success save, create db colum thumb,add image compressor/resize for save in db.thumb

--- app.py
+++ app.py
@@ -1,18 +1,38 @@
-from flask import Flask, render_template, request,jsonify
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import io
+from flask import Flask, render_template, request, jsonify, current_app as app, send_file
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.dialects.postgresql import BYTEA
 from sqlalchemy.orm import defer
 from werkzeug.utils import secure_filename
 import json
+import base64
+import codecs
+from base64 import b64encode
+from PIL import Image
+import os
+import PIL
+import glob
 
-#Start Fask Service
+# In[2]:
+
+
+# Start Fask Service
 app = Flask(__name__)
 
-#DataBase Connection postgreesql
+# DataBase Connection postgreesql
 DATABASE_URI = 'postgresql://test:test123@35.188.159.86:5432/test_db'
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+
+# In[3]:
 
 
 class Produtos(db.Model):
@@ -26,7 +46,8 @@ class Produtos(db.Model):
     preco_venda = db.Column(db.Float())
     imagem = db.Column(db.String(256))
     embalagens = db.Column(db.String(2))
-    #img = db.Column(BYTEA, unique=True, nullable=False)
+    img = db.Column(BYTEA, unique=True, nullable=False)
+    thumb = db.Column(BYTEA, unique=True, nullable=False)
 
     def to_dict(self):
         return {
@@ -37,29 +58,74 @@ class Produtos(db.Model):
             'preco_custo': self.preco_custo,
             'preco_venda': self.preco_venda,
             'imagem': self.imagem,
-            'embalagens': self.embalagens
-            #'img': self.img
+            'embalagens': self.embalagens,
+            'img': str(b64encode(self.img), 'utf-8'),
+            'thumb': '<img src="data:image/png;base64, ' + str(b64encode(self.img),
+                                                               'utf-8') + '" alt="img" style="height: 100px;" />'
         }
 
-db.create_all()
+    db.create_all()
+
+
+# In[4]:
+
+
+def convert_image(base64_str):
+    buffer = io.BytesIO()
+    imgdata = base64.b64decode(str(b64encode(base64_str), 'utf-8'))
+    # str(b64encode(self.img),'utf-8')
+    # imgdata = str(str(base64_str),'utf-8')
+    print(imgdata)
+    print(type(imgdata))
+    fixed_height = 150
+    image = Image.open(io.BytesIO(imgdata))
+    # image = Image.open(FileStorage(imgdata))
+    # image = Image.open(imgdata)
+    height_percent = (fixed_height / float(image.size[1]))
+    width_size = int((float(image.size[0]) * float(height_percent)))
+    image = image.resize((width_size, fixed_height), PIL.Image.NEAREST)
+    image.save(buffer, format="PNG")
+    img_b64 = base64.b64encode(buffer.getvalue())
+    ret = base64.b64decode(str(img_b64))
+    return ret
+
+
+# In[5]:
+
 
 @app.route('/')
 def index():
     return render_template('inicio.html', title='Inicio')
 
+
+# In[6]:
+
+
 @app.route('/produtos')
 def produtos():
     return render_template('produtos.html', title='Produtos')
+
+
+# In[7]:
+
 
 @app.route('/editar', methods=['GET'])
 def editar():
     return render_template('edit.html', title='Editar')
 
+
+# In[8]:
+
+
 @app.route('/cadastro')
 def cadastro():
     return render_template('cadastro.html', title='Cadastro')
 
-@app.route('/api/editdata',methods=['POST','GET'])
+
+# In[9]:
+
+
+@app.route('/api/editdata', methods=['POST', 'GET'])
 def editdata():
     if request.method == 'GET':
         vid = request.args.get('id')
@@ -76,21 +142,25 @@ def editdata():
         except:
             pass
         pic = request.files.get("file")
+        data = json.loads(request.form.get("data"))
         if pic:
+            rpic = pic.read()
             filename = secure_filename(pic.filename)
             print("Filename: " + str(filename))
+            result = Produtos.query.filter(Produtos.id == int(data['id'])).update({"imagem": filename})
+            result = Produtos.query.filter(Produtos.id == int(data['id'])).update({"img": rpic})
+            result = Produtos.query.filter(Produtos.id == int(data['id'])).update({"thumb": convert_image(rpic)})
         else:
             print("Pic not Exist")
             filename = None
             pic = None
-        data = json.loads(request.form.get("data"))
+
         arr = ["produto",
                'descricao',
-             'marca',
-             'preco_custo',
-             'preco_venda',
-             'imagem',
-             'embalagens']
+               'marca',
+               'preco_custo',
+               'preco_venda',
+               'embalagens']
         for x in arr:
             val = str(data[x])
             print(data['id'])
@@ -98,44 +168,66 @@ def editdata():
                 print(x + "está vazio")
                 print(val)
             else:
-                result = Produtos.query.filter(Produtos.id == int(data['id'])).update({x : val})
+                result = Produtos.query.filter(Produtos.id == int(data['id'])).update({x: val})
         db.session.commit()
-        return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
-        #return "sucess",200
+        return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
 
+
+# In[10]:
 
 
 @app.route('/api/data')
 def data():
-    rtn = {'data': [produtos.to_dict() for produtos in Produtos.query]}
-    return rtn
+    # rtn = {'data': [produtos.to_dict() for produtos in Produtos.query]}
+    prod = {'data': []}
+    for produtos in Produtos.query:
+        var = produtos.to_dict()
+        var["img"] = ""
+        prod['data'].append(var)
+    return prod
 
-@app.route('/api/save', methods=['POST'])
+
+# In[11]:
+
+
+@app.route('/api/savedata', methods=['POST'])
 def save():
     if request.method == 'POST':
-            #data = request.get_json()
-            pic = request.files.get("file")
-            data = json.loads(request.form.get("data"))
-            if pic:
-                filename = secure_filename(pic.filename)
-                #print("Filename: "+str(filename))
-            else:
-                print("Pic not Exist")
-                filename = None
-                pic = None
-            produto = Produtos(produto=str(data['produto']),
-                                descricao=str(data['descricao']),
-                                marca=str(data['marca']),
-                                preco_custo=data['preco_custo'].replace(',', '.'),
-                                preco_venda=data['preco_venda'].replace(',', '.'),
-                                imagem=str(filename),
-                                embalagens=str(data['embalagens'])#,
-                                #img=pic.read()
-                                )
-            db.session.add(produto)
-            db.session.commit()
-            return 'Sucesss', 200
+        # data = request.get_json()
+        pic = request.files.get("file")
+        data = json.loads(request.form.get("data"))
+        print(pic)
+        if pic:
+            filename = secure_filename(pic.filename)
+            # print("Filename: "+str(filename))
+            rpic = pic.read()
+        else:
+            print("Pic not Exist")
+            filename = None
+            rpic = None
+        produto = Produtos(produto=str(data['produto']),
+                           descricao=str(data['descricao']),
+                           marca=str(data['marca']),
+                           preco_custo=data['preco_custo'].replace(',', '.'),
+                           preco_venda=data['preco_venda'].replace(',', '.'),
+                           imagem=str(filename),
+                           embalagens=str(data['embalagens']),
+                           img=rpic,
+                           thumb=convert_image(rpic)
+                           )
+        db.session.add(produto)
+        db.session.commit()
+        return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
+
+
+# In[ ]:
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port=5001)
+
+# In[ ]:
+
+
+
+
